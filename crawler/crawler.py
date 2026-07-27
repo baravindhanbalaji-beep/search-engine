3eed32ef09a5c3dcaee8db28b9f2bc82af0eec83
Aravindhan <b.aravindhanbalaji@gmail.com>
2026-07-27 20:43:45 +0530

    Added metadata storage for visited websites

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,8 +1,9 @@
 import requests
+from datetime import datetime
 from collections import deque
 from .config import *
 from .parser import parse_page
-from .storage import save_page
+from .storage import save_page,save_metadata
 from .utils import same_domain
 
 class WebCrawler:
@@ -19,7 +20,9 @@ class WebCrawler:
             current_url = self.queue.popleft()
             if current_url in self.visited:
                 continue
+            page_id = len(self.visited)
             print("-"*60)
+            print(f"Page {page_id}")
             print("Visiting: ",current_url)
 
             try:
@@ -32,10 +35,19 @@ class WebCrawler:
             if response.status_code!=200:
                 continue
             self.visited.add(current_url)
-            save_page(len(self.visited),response.text)
+
+            save_page(page_id,response.text)
             title,links = parse_page(current_url,response.text)
+            #metadata
+            metadata = {
+                "id":page_id,"url":current_url,"title":title,
+                "status":response.status_code,"depth":0,"timestamp":datetime.now().isoformat()
+            }
+            save_metadata(metadata)
+
             print("Title: ",title)
             print("Links found: ",len(links))
+
             for link in links:
                 if same_domain(SEED_URL,link):
                     if link not in self.visited:
--- crawler/storage.py
+++ crawler/storage.py
@@ -1,9 +1,28 @@
 import os
+import json
 
 PAGE_DIR = "data/pages"
 os.makedirs(PAGE_DIR,exist_ok=True)
+
+META_DIR = "data/metadata"
+os.makedirs(META_DIR,exist_ok=True)
+META_FILE = os.path.join(META_DIR,"metadata.json")
 
 def save_page(page_id,html):
     filename = os.path.join(PAGE_DIR,f"{page_id}.html")
     with open(filename,"w",encoding="utf-8") as file:
         file.write(html)
+
+def save_metadata(metadata):
+    if os.path.exists(META_FILE):
+        try:
+            with open(META_FILE,"r",encoding = "utf-8") as file:
+                data = json.load(file)
+        except json.JSONDecodeError:
+            data = []
+
+    else:
+        data = []
+    data.append(metadata)
+    with open(META_FILE,"w",encoding="utf-8") as file:
+        json.dump(data,file,indent=4)
